e27d7ab6129b22a2ed337857562095192f1d8b3b
cengizhanonce <cengizhan.once@testinium.com>
2022-09-01 01:13:37 +0300

    first c

---
+++ src/main/java/deneme.java
@@ -0,0 +1,7 @@
+public class deneme {
+    public static void main(String[] args) {
+        System.out.println("*******************");
+        System.out.println("Başarılı");
+        System.out.println("*******************");
+    }
+}
